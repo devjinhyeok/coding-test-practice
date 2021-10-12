91f9f6aaeb19a6afa7caa8fffc150192b8cd75f2
aBruisedReed <snare909@gmail.com>
2021-10-12 23:52:01 +0900

    hash - 완주하지 못한 선수

--- hash/PlayerWhoNotFinish.js
+++ hash/PlayerWhoNotFinish.js
@@ -1,7 +1,27 @@
 function solution(participant, completion) {
-
+  var answer = '';
+  var map = new Map();
+  for(var i=0; i<participant.length; i++) { 
+    if(map.has(participant[i])) { 
+      map.set(participant[i], map.get(participant[i]) + 1);
+      continue;
+    } 
+    map.set(participant[i], 1);
+  }
+  for(var i=0; i<completion.length; i++) {
+    if(map.has(completion[i])) {
+      map.set(completion[i], map.get(completion[i]) - 1);
+    } else {
+      answer = completion[i];
+      continue;
+    }
+  }
+  if(answer === '') {
+    map.forEach((value, key) => {
+      if(value != 0) {
+        answer = key;
+      }
+    })
+  }
+  return answer;
 }
-
-/*
-결과 = 전체선수 - 완주
-*/
